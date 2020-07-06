b945a9656f5be6e1a1faaada15a8dbf248b77daa
skyli <skyli@192.168.0.102>
2020-07-06 19:45:46 -0300

    #several minor bug fixes #included advanced protocol filter #included a clear button with dialog message #included both jpcap library 32bits and 64bits #included new column "Appl." for the application protocol

--- src/PacketContents.java
+++ src/PacketContents.java
@@ -21,11 +21,18 @@ public class PacketContents implements PacketReceiver {
 
     @Override
     public void receivePacket(Packet packet) {
-
+        String _filterPort = "";
+        
+        
         if (packet instanceof TCPPacket) {
             tcp = (TCPPacket) packet;
+            
+            _filterPort = getProtocol(tcp.dst_port);
+            
+            if (_filterPort == "")
+                _filterPort = getProtocol(tcp.src_port);
 
-            Object[] row = {sniffer.No, tcp.length, tcp.src_ip, tcp.dst_ip, "TCP"};
+            Object[] row = {sniffer.No, tcp.length, tcp.src_ip, tcp.dst_ip, "TCP", _filterPort};
 
             rowList.add(new Object[]{sniffer.No, tcp.length, tcp.src_ip, tcp.dst_ip, "TCP", tcp.src_port, tcp.dst_port,
                 tcp.ack, tcp.ack_num, tcp.data, tcp.sequence, tcp.offset, tcp.header});
@@ -37,8 +44,8 @@ public class PacketContents implements PacketReceiver {
         } else if (packet instanceof UDPPacket) {
 
             udp = (UDPPacket) packet;
-
-            Object[] row = {sniffer.No, udp.length, udp.src_ip, udp.dst_ip, "UDP"};
+            
+            Object[] row = {sniffer.No, udp.length, udp.src_ip, udp.dst_ip, "UDP", "UDP"};
             rowList.add(new Object[]{sniffer.No, udp.length, udp.src_ip, udp.dst_ip, "UDP", udp.src_port, udp.dst_port,
                 udp.data, udp.offset, udp.header});
 
@@ -49,8 +56,8 @@ public class PacketContents implements PacketReceiver {
         } else if (packet instanceof ICMPPacket) {
 
             icmp = (ICMPPacket) packet;
-
-            Object[] row = {sniffer.No, icmp.length, icmp.src_ip, icmp.dst_ip, "ICMP"};
+            
+            Object[] row = {sniffer.No, icmp.length, icmp.src_ip, icmp.dst_ip, "ICMP", "ICMP"};
             rowList.add(new Object[]{sniffer.No, icmp.length, icmp.src_ip, icmp.dst_ip, "ICMP", icmp.checksum, icmp.header,
                 icmp.offset, icmp.orig_timestamp, icmp.recv_timestamp, icmp.trans_timestamp, icmp.data});
 
@@ -59,5 +66,32 @@ public class PacketContents implements PacketReceiver {
             sniffer.No++;
 
         }
+    }
+    
+    private String getProtocol(int port){
+        switch(port){
+            case 80:
+                return "HTTP";                
+            case 443:
+                return "HTTPS";
+            case 21:
+                return "FTP";
+            case 22:
+                return "SSH";
+            case 23:
+                return "TELNET";
+            case 25:
+                return "SMTP";
+            case 110:
+                return "POP3";
+            case 143:
+                return "IMAP";
+            case 993:
+                return "IMAPS";
+            case 53:
+                return "DNS";
+        }
+        
+        return "";        
     }
 }
